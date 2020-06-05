b9880762bb1437f9f2e3699f6485d310710396c3
houzhiping <houston@wuyoublock.com>
2020-06-05 16:21:06 +0800

    账本搭建

--- src/modules/phone/phone/element.vue
+++ src/modules/phone/phone/element.vue
@@ -4,7 +4,13 @@
       >此 Demo 在 PC 端浏览器与移动端浏览器体验差异较大，建议在 Android 或 iOS
       设备上体验。
     </nut-noticebar>
-    <h4>基本用法</h4>
+    <nut-row>
+      <nut-col :offset="6" :span="12">
+        <h4 @click="backUp">基本用法</h4>
+      </nut-col>
+    </nut-row>
+
+    <!--<nut-button type="lightred" small>去结算</nut-button>-->
     <nut-cell :showIcon="true" @click.native="switchPicker('isVisible')">
       <span slot="title"><label>日期选择</label></span>
       <span slot="sub-title">没有默认日期~~~</span>
@@ -51,6 +57,34 @@
         }}</span>
       </div>
     </nut-cell>
+    <br />
+    <nut-cell
+      :showIcon="true"
+      :isLink="true"
+      @click.native="switchPicker('isVisible4')"
+    >
+      <span slot="title">
+        <label>年选择</label>
+      </span>
+      <span slot="sub-title">单列~~~</span>
+      <div slot="desc" class="selected-option">
+        <span class="btn" @click.stop.prevent="modifyYear"
+          >修改为指定的年份</span
+        >
+        <span class="show-value">{{ year ? year : "请选择" }}</span>
+      </div>
+    </nut-cell>
+    <br />
+    <nut-cell>
+      <span slot="title">
+        <nut-textinput
+          v-model="val"
+          label="基本用法："
+          placeholder="请输入内容"
+        />
+      </span>
+    </nut-cell>
+
     <!-- demo-->
     <nut-calendar
       :is-visible.sync="isVisible"
@@ -89,6 +123,14 @@
       @choose="setChooseValue3"
     >
     </nut-calendar>
+    <!-- demo 年选择-->
+    <nut-picker
+      :is-visible="isVisible4"
+      :default-value-data="defaultValueData1"
+      :list-data="listData1"
+      @close="switchPickerClose('isVisible4')"
+      @confirm="setYearValue"
+    ></nut-picker>
   </div>
 </template>
 
@@ -97,19 +139,30 @@ import Utils from "src/utils/date.js";
 export default {
   data() {
     return {
+      isMobile: true,
       isVisible: false,
       isVisible1: false,
       isVisible2: false,
       isVisible3: false,
+      isVisible4: false,
       date: null,
       dateWeek: null,
       date1: ["2018-12-22", "2019-01-08"],
-      date2: Utils.getDay(30),
+      date2: Utils.getDay(0),
       date3: null,
+      year: null,
+      defaultValueData1: ["2020"],
+      listData1: [["2018", "2019", "2010"]],
+      val: "",
     };
   },
   methods: {
+    backUp() {
+      this.$router.push("/grid");
+      console.log(999999);
+    },
     switchPickerClose(param) {
+      this[`${param}`] = !this[`${param}`];
       console.log("close:" + param);
     },
     switchPicker(param) {
@@ -130,6 +183,12 @@ export default {
     },
     setChooseValue4(param) {},
     setChooseValue5(param) {},
+    setYearValue(chooseData) {
+      this.year = `${chooseData[0]}年`;
+    },
+    modifyYear() {
+      this.defaultValueData1 = ["2018"];
+    },
   },
 };
 </script>
